8048da6521a3ee2ad4af504cca580addd95470d1
Shafi Ahamed <shafiahamed@Shafis-MacBook-Pro.local>
2023-06-27 15:15:10 +0530

    new structure added

---
+++ src/features/loader/Loader.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+import { VscLoading } from '@react-icons/all-files/vsc/VscLoading';
+
+export const Loader = (): JSX.Element => {
+  return (
+    <React.Fragment>
+      <VscLoading />
+      <div>Hi this is Syed Shafi Ahamed i am from India</div>
+    </React.Fragment>
+  );
+};
